e3208edb65db0f8b0db5d91f3f9a32caebe11a1d
claudecc <claude007@live.cn>
2018-04-10 16:21:47 +0800

    update
    update

--- SwiftLearning/Expand/Tool/Interface.swift
+++ SwiftLearning/Expand/Tool/Interface.swift
@@ -1,19 +1,19 @@
 //
-//  LoadingView.swift
+//  Interface.swift
 //  SwiftLearning
 //
-//  Created by claudechoi on 2018/3/30.
+//  Created by claudechoi on 2018/4/10.
 //  Copyright © 2018年 蔡晓东. All rights reserved.
 //
 
 import UIKit
 
-class LoadingView: UIView {
+class Interface: NSObject {
     
     private static let instance = Interface()
     
     class func shareInstance() -> Interface {
         return instance;
     }
-
+    
 }
