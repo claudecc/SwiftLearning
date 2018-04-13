5a9639c705209b0ecb0bbddc322cbdb335575fea
claudecc <claude007@live.cn>
2018-04-13 17:25:50 +0800

    update
    update

--- SwiftLearning/Class/Me/Controller/MeVC.swift
+++ SwiftLearning/Class/Me/Controller/MeVC.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class MeVC: BaseVC {
+class MeVC: BaseVC, UITableViewDelegate, UITableViewDataSource {
 
+    private var tableView : UITableView!
+    private var listArray : NSMutableArray!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,9 +20,32 @@ class MeVC: BaseVC {
         
         self.setupUI()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
 
     func setupUI() {
         self.view.backgroundColor = UIColor.brown
+        listArray = ["1","2","3","4","5","6","7","8","9","10"]
+        tableView = UITableView(frame: self.view.frame, style: .plain)
+        self.view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+//        tableView.tableHeaderView
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.listArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cellId")
+        cell.textLabel?.text = "第\(indexPath.row)行"
+        return cell
+    }
+    
+    
+    
 }
---
+++ SwiftLearning/Class/Me/View/MeHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  MeHeaderView.swift
+//  SwiftLearning
+//
+//  Created by claudechoi on 2018/4/13.
+//  Copyright © 2018年 蔡晓东. All rights reserved.
+//
+
+import UIKit
+
+class MeHeaderView: UIView {
+    
+}
