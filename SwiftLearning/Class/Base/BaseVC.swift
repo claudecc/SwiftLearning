99f86d2a3f5756f835e83090a9a8c236bffc62a1
caixiaodong <claude007@live.cn>
2020-04-02 18:49:20 +0800

    daily

--- SwiftLearning/Class/Base/BaseVC.swift
+++ SwiftLearning/Class/Base/BaseVC.swift
@@ -15,6 +15,16 @@ class BaseVC: UIViewController {
 
         // Do any additional setup after loading the view.
         self.view.backgroundColor = UIColor.white
+        
+        if #available(iOS 13.0, *) {
+            self.overrideUserInterfaceStyle = UIUserInterfaceStyle.light
+        }
+        
+        if (self.navigationController?.viewControllers.count ?? 0) > 1 {
+            self.hidesBottomBarWhenPushed = true
+            self.createBackButton()
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,15 +32,15 @@ class BaseVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func createBackButton() {
+        let item = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(backAction))
+        self.navigationItem.leftBarButtonItem = item
     }
-    */
+    
+    @objc func backAction() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    
 
 }
